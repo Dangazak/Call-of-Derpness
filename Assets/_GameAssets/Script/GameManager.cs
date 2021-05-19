30ee864fae10d39ccbd498cb89fd8c40eb45d728
Dangazak <61272369+Dangazak@users.noreply.github.com>
2021-05-19 14:55:29 +0200

    19 mayo

--- Assets/_GameAssets/Script/GameManager.cs
+++ Assets/_GameAssets/Script/GameManager.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-   private static GameManager _instance;
+    private static GameManager _instance;
     public static GameManager Instance { get { return _instance; } }
+
+    private int vidaMax = 100;
+    private int vida = 100;
     private void Awake()
     {
         if (_instance != null && _instance != this)
---
+++ Assets/_GameAssets/Script/UI/UIManager.cs
@@ -0,0 +1,14 @@
+using System.Net.Mime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    //[SerializeField]
+    //private Slider barraVida;
+    [SerializeField]
+    private GameObject botonReiniciar;
+    //[SerializeField]
+    //private Text municion;
+}
