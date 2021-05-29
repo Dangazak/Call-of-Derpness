51204bd92492f676bbce9739f296dace125e395b
Dangazak <61272369+Dangazak@users.noreply.github.com>
2021-05-29 22:44:03 +0200

    29 mayo

---
+++ Assets/_GameAssets/Script/Enemies/PatrolManager.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PatrolManager : MonoBehaviour
+{
+    private const string PARAM_WALK = "Walking";
+    private const string PARAM_RUN = "Runing";
+    private const string PARAM_TURNLEFT = "TurningLeft";
+    private const string PARAM_TURNRIGHT = "TurningRight";
+    public bool isPlayerDetected = false;
+    private Animator agentAnimator;
+    public Transform[] patrolPoints;
+    private NavMeshAgent nma;
+    public int currentPoint = 0;
+    private bool hasDestination = false;
+    public float waitTime = 3;
+    public bool randomRoute;
+    public float walkSpeed;//Velocidad de andar
+    public float runSpeed;//Velocidad de correr
+    public float runDistance;
+    public float turnSpeed;
+    private bool turning = false;
+    public float targetAngle;
+    public float angleOffset;
+    private void Awake()
+    {
+        nma = GetComponent<NavMeshAgent>();
+        agentAnimator = GetComponentInChildren<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        nma.SetDestination(patrolPoints[currentPoint].transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (turning)
+        {
+            Rotate();
+        }
+        else if (nma.remainingDistance <= nma.stoppingDistance + 0.01f)
+        {
+            if (!hasDestination)
+            {
+                agentAnimator.SetBool(PARAM_WALK, false);
+                Invoke("SetNewDestination", waitTime);
+                hasDestination = true;
+            }
+        }
+        else
+        {
+            if (nma.remainingDistance > runDistance)
+            {
+                agentAnimator.SetBool(PARAM_RUN, true);
+                nma.speed = runSpeed;
+            }
+            else
+            {
+                agentAnimator.SetBool(PARAM_RUN, false);
+                agentAnimator.SetBool(PARAM_WALK, true);
+                nma.speed = walkSpeed;
+            }
+        }
+    }
+    void SetNewDestination()
+    {
+        hasDestination = false;
+        currentPoint++;
+        if (currentPoint == patrolPoints.Length) currentPoint = 0;
+        if (randomRoute)
+        {
+            nma.SetDestination(patrolPoints[Random.Range(0, patrolPoints.Length)].transform.position);
+        }
+        else
+        {
+            nma.SetDestination(patrolPoints[currentPoint].transform.position);
+        }
+        GetTargetAngle();
+    }
+
+    void GetTargetAngle()
+    {
+        Vector3 movementDirection = nma.steeringTarget - transform.position;
+        targetAngle = Vector3.SignedAngle(transform.forward, movementDirection, transform.up);
+        if (Mathf.Abs(targetAngle) > angleOffset && !turning)
+        {
+            turning = true;
+            nma.isStopped = true;
+            if (targetAngle > 0)
+            {
+                agentAnimator.SetBool(PARAM_TURNLEFT, true);
+                agentAnimator.SetBool(PARAM_WALK, false);
+            }
+            else
+            {
+                agentAnimator.SetBool(PARAM_TURNRIGHT, true);
+                agentAnimator.SetBool(PARAM_WALK, false);
+            }
+        }
+        else if (Mathf.Abs(targetAngle) < angleOffset && turning)
+        {
+            turning = false;
+            nma.isStopped = false;
+            agentAnimator.SetBool(PARAM_TURNLEFT, false);
+            agentAnimator.SetBool(PARAM_TURNRIGHT, false);
+        }
+    }
+    void Rotate()
+    {
+        if (targetAngle < 0)
+        {
+            transform.Rotate(0, -turnSpeed * Time.deltaTime, 0);
+        }
+        else if (targetAngle > 0)
+        {
+            transform.Rotate(0, turnSpeed * Time.deltaTime, 0);
+        }
+        GetTargetAngle();
+    }
+}
